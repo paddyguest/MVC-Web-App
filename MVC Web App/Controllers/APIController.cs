211b64eb6d06ce091554f0a8a4380d654c909034
Paddy Guest <paddy@different-class.com>
2022-02-19 16:05:22 +0000

    Part 3 tutorial series

---
+++ MVC Web App/Controllers/APIController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Web_App.Controllers
+{
+    public class APIController : Controller
+    {
+        // GET: API
+        public ActionResult GetData()
+        {
+            List<Person> Persons = new List<Person>();
+            Persons.Add(new Person { Name = "Paddy", FavouriteColour = "Blue" });
+            Persons.Add(new Person { Name = "Frank", FavouriteColour = "Brown" });
+
+            return Json(Persons, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public string FavouriteColour { get; set; }
+    }
+}
--- MVC Web App/React/src/App.js
+++ MVC Web App/React/src/App.js
@@ -1,9 +1,43 @@
-﻿import React from 'react';
+﻿import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+    const [persons, setPersons] = useState([]);
+    const APIUrl = '/API/GetData';
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        const res = await axios(
+            APIUrl
+        );
+        setPersons(res.data);
+    }
+
     return (
         <>
-           <h1>Hello, World!</h1>
+            <h1>People that we know</h1>
+            {persons && persons.length > 0 &&
+                <table style={{ borderCollapse: "collapse" }}>
+                    <thead>
+                        <tr>
+                            <th style={{ border: "solid 1px black" }}>Name</th>
+                            <th style={{ border: "solid 1px black" }}>Favourite Colour</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {persons.map(p => {
+                            return <tr key={p.Name}>
+                                <td style={{ border: "solid 1px black" }}>{p.Name}</td>
+                                <td style={{ border: "solid 1px black" }}>{p.FavouriteColour}</td>
+                            </tr>
+                        })
+                        }
+                    </tbody>
+                </table>
+            }
         </>
     );
 }
